e429ab95ee1ea30885d9ccecf0d16f4717557255
Irshandyw <yudha.irshandy@gmail.com>
2022-11-03 16:24:57 +0700

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 
 <div class="d-flex justify-content-center mt-5" style="height: 200px;" >
     <a href="contents" >
-        <button type="button"  class=" btn btn-dark" >Nilai Post-Test Kamu</button>
+        <button type="button"  class=" btn btn-dark" > Nilai Post-Test Kamu</button>
     </a>
 </div>
 
