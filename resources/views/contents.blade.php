e8b839635d730fa918cf50cf5a435d9f54082e9b
Irshandyw <yudha.irshandy@gmail.com>
2022-11-03 15:02:49 +0700

    first commit

---
+++ resources/views/contents.blade.php
@@ -0,0 +1,21 @@
+@extends('main')
+
+@section('container')
+    <div class="text-center mb-4">
+        <h3>Nilaimu Adalah.. </h3>
+        85
+    </div>
+    <div class="d-flex justify-content-center">
+        @if (($nilai >= 0) and ($nilai <= 60))
+            <div>
+                Mohon Maaf, nilai kamu belum memenuhi passing grade.
+            </div>
+        @else
+            <div>
+               Pertahankan! Nilaimu diatas rata-rata
+            </div>
+        @endif
+    </div>
+
+@endsection
+
---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+  <footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50 justify-content-center mb-5" >
+    <div class="container text-center">
+      <small>Copyright &copy; Bootcamp</small>
+    </div>
+  </footer>
+</body>
